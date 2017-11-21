59bc232ea3414007476c8199e8dc4f83ac92d261
Ken J. Dellosa <dellosaken6@gmail.com>
2017-11-21 11:26:27 +0800

    Changed the navbar

--- front-end/src/components/startpage.js
+++ front-end/src/components/startpage.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 import {Navbar, NavItem, Icon} from 'react-materialize'
+
 export default class Home extends Component {
-  render(){
+  render(props){
     return(
-      <Navbar brand='logo' right>
-      	<NavItem href='get-started.html'><Icon>search</Icon></NavItem>
-      	<NavItem href='get-started.html'><Icon>view_module</Icon></NavItem>
-      	<NavItem href='get-started.html'><Icon>refresh</Icon></NavItem>
-      	<NavItem href='get-started.html'><Icon>more_vert</Icon></NavItem>
+      <Navbar className="red darken-4" brand="Elbi Quiz" right>
+      	<NavItem href='#'><Icon>search</Icon></NavItem>
+      	<NavItem href='#'><Icon>view_module</Icon></NavItem>
       </Navbar>
     )
   }

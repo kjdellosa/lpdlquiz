d37b9416c689cc09ef6aa9abf7f5cbc145880c25
Ken J. Dellosa <dellosaken6@gmail.com>
2017-11-21 12:50:09 +0800

    Homepage done

--- front-end/src/components/startpage.js
+++ front-end/src/components/startpage.js
@@ -23,6 +23,79 @@ export default class Home extends Component {
             <br></br>
           </div>
         </div>
+
+        <div class="container">
+          <div class="section">
+            <div class="row">
+              <div class="col m5ths s6">
+                <div class="icon-block">
+                <h2 class="center red-text text-darken-4"><i class="material-icons">date_range</i></h2>
+                <h5 class="center">History</h5>
+
+                <p class="light"></p>
+                </div>
+              </div>
+              <div class="col m5ths s6">
+                <div class="icon-block">
+                <h2 class="center red-text text-darken-4"><i class="material-icons">lightbulb_outline</i></h2>
+                <h5 class="center">General Knowledge</h5>
+
+                <p class="light"></p>
+                </div>
+              </div>
+              <div class="col m5ths s6">
+                <div class="icon-block">
+                <h2 class="center red-text text-darken-4"><i class="material-icons">local_bar</i></h2>
+                <h5 class="center">Nightlife</h5>
+
+                <p class="light"></p>
+                </div>
+              </div>
+              <div class="col m5ths s6">
+                <div class="icon-block">
+                <h2 class="center red-text text-darken-4"><i class="material-icons">restaurant</i></h2>
+                <h5 class="center">Kainan</h5>
+
+                <p class="light"></p>
+                </div>
+              </div>
+              <div class="col m5ths s6">
+                <div class="icon-block">
+                <h2 class="center red-text text-darken-4"><i class="material-icons">layers</i></h2>
+                <h5 class="center">Geography</h5>
+
+                <p class="light"></p>
+                </div>
+              </div>
+          </div>
+        </div>
+        <br></br>
+        </div>
+
+        <footer class="page-footer red darken-4">
+      <div class="container">
+        <div class="row">
+          <div class="col l6 s12">
+            <h5 class="white-text">LPDL</h5>
+            <p class="grey-text text-lighten-4">CMSC 100 EF-1L 1st Semester 2017-2018</p>
+          </div>
+          <div class="col l4 s12">
+            <h5 class="white-text">Authors</h5>
+            <ul>
+              <li><a class="white-text" href="#!">Legaspi, Stephanie Y.</a></li>
+              <li><a class="white-text" href="#!">Pamintuan, Jolicia Aicila Marie R.</a></li>
+              <li><a class="white-text" href="#!">Dellosa, Ken J.</a></li>
+              <li><a class="white-text" href="#!">Leal, Merry Trizia M.</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="footer-copyright">
+        <div class="container">
+        Mercy and Compassion please
+        </div>
+      </div>
+    </footer>
       </div>
     )
   }

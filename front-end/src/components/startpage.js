60f6f3a3604129aa559ff0aae8b9872b868791f3
Ken J. Dellosa <dellosaken6@gmail.com>
2017-11-21 11:40:16 +0800

    Added splash message

--- front-end/src/components/startpage.js
+++ front-end/src/components/startpage.js
@@ -4,10 +4,26 @@ import {Navbar, NavItem, Icon} from 'react-materialize'
 export default class Home extends Component {
   render(props){
     return(
-      <Navbar className="red darken-4" brand="Elbi Quiz" right>
-      	<NavItem href='#'><Icon>search</Icon></NavItem>
-      	<NavItem href='#'><Icon>view_module</Icon></NavItem>
-      </Navbar>
+      <div>
+        <Navbar className="red darken-4" brand="Elbi Quiz" right>
+        	<NavItem href='#'>Manage</NavItem>
+        </Navbar>
+
+        <div class="section no-pad-bot" id="index-banner">
+          <div class="container">
+            <br></br>
+            <h1 class="header center red-text">The Elbi Quiz!</h1>
+            <div class="row center">
+            <h5 class="header col s12 light">A general information quiz about Los Banos</h5>
+          </div>
+
+          <div class="row center">
+            <a href="#" id="download-button" class="btn-large waves-effect waves-light red">Start the Quiz!</a>
+            </div>
+            <br></br>
+          </div>
+        </div>
+      </div>
     )
   }
 }

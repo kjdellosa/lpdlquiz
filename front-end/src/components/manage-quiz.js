5623f705e1bb588aad651bc67344402bc81748e9
Ken J. Dellosa <dellosaken6@gmail.com>
2017-11-26 14:43:47 +0800

    Added manage to navbar as a dropdown

--- front-end/src/components/manage-quiz.js
+++ front-end/src/components/manage-quiz.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 import Nav from './navigation.js';
 import Footer from './footer.js';
+import {Collection, CollectionItem, Icon, SideNavItem, SideNav, Button} from 'react-materialize';
 
 export default class Home extends Component{
   render(props){
     return(
       <div>
         <Nav />
-        <Footer />
+
+        <div id="pagecontent">
+          
+        </div>
+
+        <Footer/>
       </div>
     )
   }
--- front-end/src/components/navigation.js
+++ front-end/src/components/navigation.js
@@ -1,13 +1,35 @@
 import React, { Component } from 'react';
-import {Navbar, NavItem, Icon} from 'react-materialize'
+import {Navbar, NavItem, Icon, SideNav, SideNavItem, Button,
+Dropdown, Manage} from 'react-materialize'
 
 export default class Home extends Component{
   render(props){
     return(
       <div>
-      <Navbar className="red darken-2" brand="Elbi Quiz" right>
-        <NavItem href="/manage">Manage</NavItem>
-      </Navbar>
+      <header>
+
+      <ul id="dropdown1" class="dropdown-content">
+      <li><a href="#!" class="black-text text-darken-1">View/Edit Questions</a></li>
+      <li><a href="#!" class="black-text text-darken-1">Add Question</a></li>
+      <li><a href="#!" class="black-text text-darken-1">Delete Question</a></li>
+      <li class="divider"></li>
+      <li><a href="#!" class="black-text text-darken-1">View/Edit Categories</a></li>
+      <li><a href="#!" class="black-text text-darken-1">Add Category</a></li>
+      <li><a href="#!" class="black-text text-darken-1">Delete Category</a></li>
+
+      </ul>
+
+      <nav class="red darken-3">
+      <div class="nav-wrapper">
+        <a href="#!" class="brand-logo">Elbi Quiz</a>
+        <ul class="right hide-on-med-and-down">
+          <li><a class="dropdown-button" href="#!" data-activates="dropdown1"
+          data-beloworigin="true" data-constrainWidth="false">Manage<i class="material-icons right">arrow_drop_down</i></a></li>
+        </ul>
+      </div>
+      </nav>
+
+      </header>
       </div>
     )
   }
